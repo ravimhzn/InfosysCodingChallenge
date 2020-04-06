75462c7fc5d47c7e953b937d0ef1afbb0f5196be
Ravi Maharjan <ravi.maharjan2018@gmail.com>
2020-04-06 12:56:56 +1000

    Datasource setup complete for api network call

--- app/src/main/java/com/ravimhzn/infosyscodingapplication/ui/fragments/AboutFrag.kt
+++ app/src/main/java/com/ravimhzn/infosyscodingapplication/ui/fragments/AboutFrag.kt
@@ -1,16 +1,25 @@
 package com.ravimhzn.infosyscodingapplication.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ravimhzn.infosyscodingapplication.R
+import com.ravimhzn.infosyscodingapplication.ui.viemodels.AboutFragViewModel
+import com.ravimhzn.infosyscodingapplication.utils.data.map
 import com.ravimhzn.openweatherapp.modules.BaseFragment
+import javax.inject.Inject
 
 
 class AboutFrag : BaseFragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel: AboutFragViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +27,20 @@ class AboutFrag : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        startObservingForData()
+    }
+
+    private fun startObservingForData() {
+        viewModel.countryResult.removeObservers(viewLifecycleOwner)
+        viewModel.countryResult.observe(viewLifecycleOwner, Observer { it ->
+            it.map {
+                println("Debug -> Title:: ${it.title}")
+            }
+        })
     }
 
 }
